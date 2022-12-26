02cc80a803719c20451dcf91cd4c9082998f7f91
Bazly Anan <bazley421@gmail.com>
2022-12-26 21:53:49 +0600

    project upload

---
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('/image/logo/mylogo.png') }}" alt="" style="width: 100px;">
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('/image/logo/mylogo.png') }}" alt="" style="width: 100px;">
+</a>
